5832b7cac6bf42acf5f1cdb178e1b710a57ace44
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-07-06 15:43:29 +0700

    hackerrankasd

--- HackerNews/src/app/app.component.ts
+++ HackerNews/src/app/app.component.ts
@@ -1,58 +1,18 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  // article = {
-  //   title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
-  //   url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
-  // };
-  articles = [
-    {
-      title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
-      url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
-    },
-    {
-      title: 'Game of Life',
-      url: 'https://thefullsnack.com/posts/game-of-life.html'
-    },
-    {
-      title: 'Nguyên tắc thiết kế REST API',
-      url: 'https://medium.com/eway/nguyên-tắc-thiết-kế-rest-api-23add16968d7'
-    },
-    {
-      title: 'Why You Only Need to Test with 5 Users',
-      url: 'https://www.nngroup.com/articles/why-you-only-need-to-test-with-5-users/'
-    },
-    {
-      title: 'Let’s Build A Web Server. Part 1.',
-      url: 'https://ruslanspivak.com/lsbaws-part1/'
-    }
-  ];
-  // updateArticle() {
-  //   this.article.title = document.getElementById("article-title").value;
-  //   this.article.url = document.getElementById("article-url").value;
-  // }
-  addArticle() {
-    let title = document.getElementById("article-title").value;
-    let url = document.getElementById("article-url").value;
-    let article={
-        title: title,
-        url: url
-      };
-    this.articles.push(article);
-    document.getElementById("article-url").value="";
-    document.getElementById("article-title").value="";
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
   }
 
-  deleteArticle() {
+}
 
-  }
-
-  editArticle() {
-
-  }
+export class BodyWebComponent {
 }
--- HackerNews/src/app/app.module.ts
+++ HackerNews/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import { LikesComponent } from './likes/likes.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyWebComponent } from './body-web/body-web.component';
+import { SidabarComponent } from './sidebar/sidabar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LikesComponent
+    LikesComponent,
+    NavbarComponent,
+    FooterComponent,
+    BodyWebComponent,
+    SidabarComponent
   ],
   imports: [
     BrowserModule,
